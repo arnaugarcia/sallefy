1e95e28e98995d88016f2b52e1da9e50c41dd078
Arnau Garcia <agarciag@opentrends.net>
2019-09-18 14:29:51 +0200

    Created LikeService and deleted LikeTrack Service

---
+++ src/main/java/com/sallefy/service/LikeService.java
@@ -0,0 +1,19 @@
+package com.sallefy.service;
+
+import com.sallefy.service.dto.LikeTrackDTO;
+
+import java.util.Optional;
+
+/**
+ * Service Interface for managing likes in Albums and Tracks.
+ */
+public interface LikeService {
+
+    /**
+     * Method to like a Track by "id"
+     *
+     * @param trackId the id of the Track
+     * @return a LikeTrackDTO with the liked boolean
+     */
+    Optional<LikeTrackDTO> toggleLikeTrack(Long trackId);
+}
