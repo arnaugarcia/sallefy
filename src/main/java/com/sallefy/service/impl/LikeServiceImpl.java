26276247842302f0697d9858368bebea44f74143
Arnau Garcia <agarciag@opentrends.net>
2019-09-19 09:50:01 +0200

    Added album and track repositories to like service

--- src/main/java/com/sallefy/service/impl/LikeServiceImpl.java
+++ src/main/java/com/sallefy/service/impl/LikeServiceImpl.java
@@ -1,11 +1,10 @@
 package com.sallefy.service.impl;
 
+import com.sallefy.repository.LikeAlbumRepository;
 import com.sallefy.repository.LikeTrackRepository;
 import com.sallefy.security.SecurityUtils;
 import com.sallefy.service.LikeService;
 import com.sallefy.service.dto.LikeTrackDTO;
-import com.sallefy.web.rest.errors.BadRequestAlertException;
-import io.undertow.util.BadRequestException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,13 +14,16 @@ public class LikeServiceImpl implements LikeService {
 
     private final LikeTrackRepository likeTrackRepository;
 
-    public LikeServiceImpl(LikeTrackRepository likeTrackRepository) {
+    private final LikeAlbumRepository likeAlbumRepository;
+
+    public LikeServiceImpl(LikeTrackRepository likeTrackRepository, LikeAlbumRepository likeAlbumRepository) {
         this.likeTrackRepository = likeTrackRepository;
+        this.likeAlbumRepository = likeAlbumRepository;
     }
 
     @Override
-    public Optional<LikeTrackDTO> toggleLikeTrack(Long trackId) throws BadRequestException {
-        String login = SecurityUtils.getCurrentUserLogin().orElseThrow(BadRequestException::new);
+    public Optional<LikeTrackDTO> toggleLikeTrack(Long trackId) {
+        String login = SecurityUtils.getCurrentUserLogin().get();
         System.out.println(login);
         return Optional.empty();
     }
