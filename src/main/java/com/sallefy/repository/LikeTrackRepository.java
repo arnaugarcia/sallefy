b6df6a513acaa4b9faf504253ec1ccff9011e1fb
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-21 16:28:16 +0200

    Added optional to findTrackByUser

--- src/main/java/com/sallefy/repository/LikeTrackRepository.java
+++ src/main/java/com/sallefy/repository/LikeTrackRepository.java
@@ -1,9 +1,12 @@
 package com.sallefy.repository;
 import com.sallefy.domain.LikeTrack;
+import com.sallefy.domain.Track;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the LikeTrack entity.
@@ -14,5 +17,8 @@ public interface LikeTrackRepository extends JpaRepository<LikeTrack, Long> {
 
     @Query("select likeTrack from LikeTrack likeTrack where likeTrack.user.login = ?#{principal.username}")
     List<LikeTrack> findByUserIsCurrentUser();
+
+    @Query("select likeTrack from LikeTrack likeTrack where likeTrack.user.login = ?#{principal.username} and likeTrack.track = :track")
+    Optional<LikeTrack> findTrackByUserIsCurrentUser(@Param("track") Track track);
 
 }
