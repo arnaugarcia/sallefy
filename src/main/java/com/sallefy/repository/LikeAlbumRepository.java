e4e13f9c830a0a8757126d14b51b4345f5c51489
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-22 14:10:30 +0200

    Added query for finding an album by current user

--- src/main/java/com/sallefy/repository/LikeAlbumRepository.java
+++ src/main/java/com/sallefy/repository/LikeAlbumRepository.java
@@ -1,9 +1,13 @@
 package com.sallefy.repository;
+
+import com.sallefy.domain.Album;
 import com.sallefy.domain.LikeAlbum;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the LikeAlbum entity.
@@ -15,4 +19,6 @@ public interface LikeAlbumRepository extends JpaRepository<LikeAlbum, Long> {
     @Query("select likeAlbum from LikeAlbum likeAlbum where likeAlbum.user.login = ?#{principal.username}")
     List<LikeAlbum> findByUserIsCurrentUser();
 
+    @Query("select likeAlbum from LikeAlbum likeAlbum where likeAlbum.user.login = ?#{principal.username} and likeAlbum.album = :album ")
+    Optional<LikeAlbum> findAlbumByUserIsCurrentUser(@Param("album") Album album);
 }
