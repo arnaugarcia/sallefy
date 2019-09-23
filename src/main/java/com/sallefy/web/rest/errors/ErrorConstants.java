5a8e0be76b1426f3ecb446d3a59dc614c4947bbf
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-23 10:21:07 +0200

    Fixed slash for not-found constant

--- src/main/java/com/sallefy/web/rest/errors/ErrorConstants.java
+++ src/main/java/com/sallefy/web/rest/errors/ErrorConstants.java
@@ -7,7 +7,7 @@ public final class ErrorConstants {
     public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
     public static final String ERR_VALIDATION = "error.validation";
     public static final String ERR_ALBUM_NOT_FOUND = "error.album.notFound";
-    public static final String PROBLEM_BASE_URL = "http://sallefy.eu-west-3.elasticbeanstalk.com/problem/";
+    public static final String PROBLEM_BASE_URL = "http://sallefy.eu-west-3.elasticbeanstalk.com/problem";
     public static final URI DEFAULT_TYPE = URI.create(PROBLEM_BASE_URL + "/problem-with-message");
     public static final URI CONSTRAINT_VIOLATION_TYPE = URI.create(PROBLEM_BASE_URL + "/constraint-violation");
     public static final URI NOT_FOUND_TYPE = URI.create(PROBLEM_BASE_URL + "/not-found");
