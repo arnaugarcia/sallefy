4eda9c1ca82f16f23d3b101bd8cb575a1aba625a
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-23 10:24:13 +0200

    Removed hard-coded exceptions for album

--- src/main/java/com/sallefy/web/rest/errors/AlbumNotFoundException.java
+++ src/main/java/com/sallefy/web/rest/errors/AlbumNotFoundException.java
@@ -3,7 +3,7 @@ package com.sallefy.web.rest.errors;
 public class AlbumNotFoundException extends NotFoundAlertException {
 
     public AlbumNotFoundException() {
-        super("Album not found", "Track", "notFound");
+        super("Album not found", "Album", ErrorConstants.ERR_ALBUM_NOT_FOUND);
     }
 
 }
