7e58e06bcda6a93607b5f09f97e5eb7a4d7f017e
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-23 10:37:17 +0200

    Changed implementation of TrackNotFound to NotFoundAlertException

--- src/main/java/com/sallefy/web/rest/errors/TrackNotFoundException.java
+++ src/main/java/com/sallefy/web/rest/errors/TrackNotFoundException.java
@@ -1,9 +1,9 @@
 package com.sallefy.web.rest.errors;
 
-public class TrackNotFoundException extends RuntimeException {
+public class TrackNotFoundException extends NotFoundAlertException {
 
     public TrackNotFoundException() {
-        super("Track not found");
+        super("Track not found", "Track", ErrorConstants.ERR_TRACK_NOT_FOUND);
     }
 
 }
