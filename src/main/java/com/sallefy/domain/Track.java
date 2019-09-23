cb75ef9e9cae0858a55221c547161c6abf471dbe
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-23 15:04:09 +0200

    Added Id to genre DTO

--- src/main/java/com/sallefy/domain/Track.java
+++ src/main/java/com/sallefy/domain/Track.java
@@ -1,8 +1,11 @@
 package com.sallefy.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 
@@ -40,6 +43,7 @@ public class Track implements Serializable {
     @Column(name = "thumbnail")
     private String thumbnail;
 
+    @CreationTimestamp
     @Column(name = "created_at")
     private ZonedDateTime createdAt;
 
