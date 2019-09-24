fcfe140cec05bb4a518894f93f9bdb36bb48391d
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-09-24 12:58:48 +0200

    Removed unnecessary fields of TrackDTO

--- src/main/java/com/sallefy/service/dto/TrackDTO.java
+++ src/main/java/com/sallefy/service/dto/TrackDTO.java
@@ -14,25 +14,15 @@ public class TrackDTO implements Serializable {
 
     private String name;
 
-    private String rating;
-
     private String url;
 
-    private String popularity;
-
     private String thumbnail;
-
-    private ZonedDateTime createdAt;
 
     private ZonedDateTime released;
 
     private Integer duration;
 
     private String color;
-
-    private Long userId;
-
-    private String userLogin;
 
     private Set<GenreDTO> genres = new HashSet<>();
 
@@ -52,14 +42,6 @@ public class TrackDTO implements Serializable {
         this.name = name;
     }
 
-    public String getRating() {
-        return rating;
-    }
-
-    public void setRating(String rating) {
-        this.rating = rating;
-    }
-
     public String getUrl() {
         return url;
     }
@@ -68,28 +50,12 @@ public class TrackDTO implements Serializable {
         this.url = url;
     }
 
-    public String getPopularity() {
-        return popularity;
-    }
-
-    public void setPopularity(String popularity) {
-        this.popularity = popularity;
-    }
-
     public String getThumbnail() {
         return thumbnail;
     }
 
     public void setThumbnail(String thumbnail) {
         this.thumbnail = thumbnail;
-    }
-
-    public ZonedDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(ZonedDateTime createdAt) {
-        this.createdAt = createdAt;
     }
 
     public ZonedDateTime getReleased() {
@@ -114,22 +80,6 @@ public class TrackDTO implements Serializable {
 
     public void setColor(String color) {
         this.color = color;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserLogin() {
-        return userLogin;
-    }
-
-    public void setUserLogin(String userLogin) {
-        this.userLogin = userLogin;
     }
 
     public Set<GenreDTO> getGenres() {
@@ -166,16 +116,11 @@ public class TrackDTO implements Serializable {
         return "TrackDTO{" +
             "id=" + getId() +
             ", name='" + getName() + "'" +
-            ", rating='" + getRating() + "'" +
             ", url='" + getUrl() + "'" +
-            ", popularity='" + getPopularity() + "'" +
             ", thumbnail='" + getThumbnail() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
             ", released='" + getReleased() + "'" +
             ", duration=" + getDuration() +
             ", color='" + getColor() + "'" +
-            ", user=" + getUserId() +
-            ", user='" + getUserLogin() + "'" +
             "}";
     }
 }
