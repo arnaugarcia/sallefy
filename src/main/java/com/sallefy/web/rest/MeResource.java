63569ee68801c24f9557f1d8827bf0a6c5a7c0d2
Arnau Garcia <agarciag@opentrends.net>
2019-10-03 11:37:18 +0200

    Added endpoints for getting the current user data

--- src/main/java/com/sallefy/web/rest/MeResource.java
+++ src/main/java/com/sallefy/web/rest/MeResource.java
@@ -1,7 +1,9 @@
 package com.sallefy.web.rest;
 
 import com.sallefy.service.TrackService;
+import com.sallefy.service.dto.AlbumDTO;
 import com.sallefy.service.dto.TrackDTO;
+import com.sallefy.service.dto.UserDTO;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -57,6 +59,45 @@ public class MeResource {
     }
 
     /**
+     * {@code GET  /me/tracks/:id} : get the track by "id".
+     *
+     * @param id the id of the track to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the track, or with status {@code 404 (Not Found)}.
+     */
+    @ApiOperation(
+        notes = "Find own track by id",
+        value = "Shows the desired track by the requested id"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successful operation"),
+        @ApiResponse(code = 403, message = "You're not the owner of the requested source"),
+        @ApiResponse(code = 404, message = "Playlist not found"),
+    })
+    @GetMapping("/me/tracks/{id}")
+    public ResponseEntity<TrackDTO> getOwnTrackById(@PathVariable Long id) {
+        log.debug("REST request to get a Track with id: {}", id);
+        throw new NotYetImplementedException("Not yet implemented");
+    }
+
+    /**
+     * {@code GET  /me/tracks/liked} : get the liked tracks.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of the liked track in the body.
+     */
+    @ApiOperation(
+        notes = "Find liked tracks",
+        value = "Shows al the liked tracks by the current user"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successful operation")
+    })
+    @GetMapping("/me/tracks/liked")
+    public ResponseEntity<List<TrackDTO>> getLikedTracks() {
+        log.debug("REST request to get the list of liked Tracks");
+        throw new NotYetImplementedException("Not yet implemented");
+    }
+
+    /**
      * {@code GET  /me/albums} : get all the albums of the current user.
      *
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of albums in body.
@@ -75,13 +116,52 @@ public class MeResource {
     }
 
     /**
-     * {@code GET  /me/playlists} : get all the playlists of the current user.
+     * {@code GET  /me/albums/:id} : get the album by "id".
      *
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of albums in body.
+     * @param id the id of the album to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the album, or with status {@code 404 (Not Found)}.
      */
     @ApiOperation(
-        value = "Shows own albums",
-        notes = "Returns all the albums, including the private ones. If the current user has ADMIN role, shows all the albums of all users"
+        notes = "Find own album by id",
+        value = "Shows the desired album by the requested id"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successful operation"),
+        @ApiResponse(code = 403, message = "You're not the owner of the requested source"),
+        @ApiResponse(code = 404, message = "Playlist not found"),
+    })
+    @GetMapping("/me/albums/{id}")
+    public ResponseEntity<TrackDTO> getOwnAlbumById(@PathVariable Long id) {
+        log.debug("REST request to get a Album with id: {}", id);
+        throw new NotYetImplementedException("Not yet implemented");
+    }
+
+    /**
+     * {@code GET  /me/albums/liked} : get the liked albums.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of the liked albums in the body.
+     */
+    @ApiOperation(
+        notes = "Find liked albums",
+        value = "Shows al the liked albums by the current user"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successful operation")
+    })
+    @GetMapping("/me/albums/liked")
+    public ResponseEntity<List<AlbumDTO>> getLikedAlbums() {
+        log.debug("REST request to get the list of liked Albums");
+        throw new NotYetImplementedException("Not yet implemented");
+    }
+
+    /**
+     * {@code GET  /me/playlists} : get all the playlists of the current user.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of playlists in body.
+     */
+    @ApiOperation(
+        value = "Shows own playlists",
+        notes = "Returns all the playlists, including the private ones. If the current user has ADMIN role, shows all the albums of all users"
     )
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "Successful operation")
@@ -108,8 +188,62 @@ public class MeResource {
         @ApiResponse(code = 404, message = "Playlist not found"),
     })
     @GetMapping("/me/playlists/{id}")
-    public ResponseEntity<TrackDTO> getPlaylist(@PathVariable Long id) {
+    public ResponseEntity<TrackDTO> getOwnPlaylist(@PathVariable Long id) {
         log.debug("REST request to get a Playlist with id: {}", id);
+        throw new NotYetImplementedException("Not yet implemented");
+    }
+
+    /**
+     * {@code GET  /me/playlists/followed} : get the followed playlists.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of the followed playlists in the body.
+     */
+    @ApiOperation(
+        notes = "Find followed playlists",
+        value = "Shows al the followed playlists by the current user"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successful operation")
+    })
+    @GetMapping("/me/tracks/followed")
+    public ResponseEntity<List<TrackDTO>> getFollowedTracks() {
+        log.debug("REST request to get the list of followed Playlists");
+        throw new NotYetImplementedException("Not yet implemented");
+    }
+
+    /**
+     * {@code GET  /me/users/following} : get the following users.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of the following users in the body.
+     */
+    @ApiOperation(
+        notes = "Find following users",
+        value = "Shows all the following users by the current user"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successful operation")
+    })
+    @GetMapping("/me/users/followings")
+    public ResponseEntity<List<UserDTO>> getFollowingUsers() {
+        log.debug("REST request to get the list of following Users");
+        throw new NotYetImplementedException("Not yet implemented");
+    }
+
+    /**
+     * {@code GET  /me/users/followers} : get the current user followers.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of the current user followers in the body.
+     */
+    @ApiOperation(
+        notes = "Find followers of the current user",
+        value = "Shows all the current user followers"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successful operation")
+    })
+    @GetMapping("/me/users/followers")
+    public ResponseEntity<List<UserDTO>> getFollowersOfTheUser() {
+        log.debug("REST request to get the list of the current user followers");
         throw new NotYetImplementedException("Not yet implemented");
     }
 
