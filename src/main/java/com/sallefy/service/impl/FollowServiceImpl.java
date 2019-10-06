213c1bc6c9f83b5189f51254612456d1b3ed356c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-10-06 18:11:06 +0200

    Finished logic to follow a playlist

--- src/main/java/com/sallefy/service/impl/FollowServiceImpl.java
+++ src/main/java/com/sallefy/service/impl/FollowServiceImpl.java
@@ -11,6 +11,8 @@ import com.sallefy.service.dto.PlaylistDTO;
 import com.sallefy.service.mapper.PlaylistMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class FollowServiceImpl implements FollowService {
 
@@ -38,16 +40,22 @@ public class FollowServiceImpl implements FollowService {
 
         findPlaylistById(playlistId);
 
+        final Optional<FollowPlaylist> followedPlaylist = followPlaylistRepository.findByPlaylistAndCurrentUser(playlistId);
+
         FollowDTO followDTO = new FollowDTO();
 
-        FollowPlaylist followPlaylist = new FollowPlaylist();
 
-        followPlaylist.setUser(currentUser);
-        followPlaylist.setPlaylist(playlistMapper.fromId(playlistId));
+        if (followedPlaylist.isPresent()) {
+            followPlaylistRepository.delete(followedPlaylist.get());
+            followDTO.setFollowed(false);
+        } else {
+            FollowPlaylist followPlaylist = new FollowPlaylist();
+            followPlaylist.setUser(currentUser);
+            followPlaylist.setPlaylist(playlistMapper.fromId(playlistId));
+            followPlaylistRepository.save(followPlaylist);
+            followDTO.setFollowed(true);
+        }
 
-        followPlaylistRepository.save(followPlaylist);
-
-        followDTO.setFollowed(true);
 
         return followDTO;
     }
