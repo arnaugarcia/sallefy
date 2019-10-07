35c34743d2520d0bb42797ffafacd597a501a456
Arnau Garcia <agarciag@opentrends.net>
2019-10-07 10:07:05 +0200

    Added query to check if user is being followed by login

--- src/main/java/com/sallefy/repository/FollowUserRepository.java
+++ src/main/java/com/sallefy/repository/FollowUserRepository.java
@@ -1,9 +1,11 @@
 package com.sallefy.repository;
 import com.sallefy.domain.FollowUser;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the FollowUser entity.
@@ -17,5 +19,8 @@ public interface FollowUserRepository extends JpaRepository<FollowUser, Long> {
 
     @Query("select followUser from FollowUser followUser where followUser.user.login = ?#{principal.username}")
     List<FollowUser> findByUserIsCurrentUser();
+
+    @Query("select followUser from FollowUser followUser where followUser.user.login = ?#{principal.username} and followUser.followed.login = :login")
+    Optional<FollowUser> findIfFollowedUserIsFollowedByCurrentUser(@Param("login") String login);
 
 }
