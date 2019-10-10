2279343cf1dd52db6cd66a4955a7c2b51d52a3d0
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-10-10 09:03:31 +0200

    Added logic for playing a track

--- src/main/java/com/sallefy/service/impl/PlayServiceImpl.java
+++ src/main/java/com/sallefy/service/impl/PlayServiceImpl.java
@@ -6,20 +6,24 @@ import com.sallefy.repository.PlaybackRepository;
 import com.sallefy.service.PlayService;
 import com.sallefy.service.TrackService;
 import com.sallefy.service.UserService;
+import com.sallefy.service.mapper.TrackMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlayServiceImpl implements PlayService {
 
     private final PlaybackRepository playbackRepository;
+    private final TrackMapper trackMapper;
 
     private final TrackService trackService;
     private final UserService userService;
 
     public PlayServiceImpl(PlaybackRepository playbackRepository,
+                           TrackMapper trackMapper,
                            TrackService trackService,
                            UserService userService) {
         this.playbackRepository = playbackRepository;
+        this.trackMapper = trackMapper;
         this.trackService = trackService;
         this.userService = userService;
     }
@@ -28,10 +32,16 @@ public class PlayServiceImpl implements PlayService {
     public void playTrack(Long id) {
         final User currentUser = userService.getUserWithAuthorities();
 
-        trackService.findOne(id);
+        checkIfTrackExists(id);
 
         Playback playback = new Playback();
-        playback.setTrack();
+        playback.setTrack(trackMapper.fromId(id));
         playback.setUser(currentUser);
+
+        playbackRepository.save(playback);
+    }
+
+    private void checkIfTrackExists(Long id) {
+        trackService.findOne(id);
     }
 }
