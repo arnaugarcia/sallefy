8b6303a57212e56baa8280adf0e3df49a808f938
Arnau Garcia <agarciag@opentrends.net>
2019-10-11 12:00:12 +0200

    Created method to add user agent to playback request

--- src/main/java/com/sallefy/service/impl/PlayServiceImpl.java
+++ src/main/java/com/sallefy/service/impl/PlayServiceImpl.java
@@ -3,6 +3,7 @@ package com.sallefy.service.impl;
 import com.sallefy.domain.Playback;
 import com.sallefy.domain.Track;
 import com.sallefy.domain.User;
+import com.sallefy.domain.enumeration.AgentType;
 import com.sallefy.repository.PlaybackRepository;
 import com.sallefy.repository.TrackRepository;
 import com.sallefy.service.LocationService;
@@ -13,6 +14,9 @@ import com.sallefy.service.exception.TrackNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static com.sallefy.domain.enumeration.AgentType.*;
+import static org.springframework.util.StringUtils.hasText;
 
 @Service
 public class PlayServiceImpl implements PlayService {
@@ -25,6 +29,8 @@ public class PlayServiceImpl implements PlayService {
 
     private final LocationService locationService;
 
+    private final String USER_AGENT_HEADER_KEY = "user-agent";
+
     public PlayServiceImpl(PlaybackRepository playbackRepository,
                            TrackRepository trackRepository,
                            UserService userService,
@@ -36,18 +42,19 @@ public class PlayServiceImpl implements PlayService {
     }
 
     @Override
-    public void playTrack(HttpServletRequest requestContext, Long id) {
+    public void playTrack(HttpServletRequest request, Long id) {
         final User currentUser = userService.getUserWithAuthorities();
 
         Track track = findTrackById(id);
 
-        final LocationDTO locationDTO = locationService.locate(requestContext);
+        final LocationDTO locationDTO = locationService.locate(request);
 
         Playback playback = new Playback();
         playback.setUser(currentUser);
         playback.setTrack(track);
         playback.setLatitude(locationDTO.getLatitude());
         playback.setLongitude(locationDTO.getLongitude());
+        playback.setAgent(transformAgentType(request.getHeader(USER_AGENT_HEADER_KEY)));
         playback.setIp(locationDTO.getIp());
 
         playbackRepository.save(playback);
@@ -56,6 +63,19 @@ public class PlayServiceImpl implements PlayService {
     private Track findTrackById(Long id) {
         return trackRepository.findById(id)
             .orElseThrow(TrackNotFoundException::new);
+    }
+
+    private AgentType transformAgentType(String value) {
+
+        if (!hasText(value)) {
+            return OTHER;
+        }
+
+        if (value.contains("Mobile")) {
+            return MOBILE;
+        } else {
+            return WEB;
+        }
     }
 
 }
