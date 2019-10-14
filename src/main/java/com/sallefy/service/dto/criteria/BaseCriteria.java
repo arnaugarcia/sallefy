0a0b12bf578373fdc1c745550d1ec97a883467d3
Arnau Garcia <agarciag@opentrends.net>
2019-10-14 14:23:01 +0200

    Created BaseCriteria for handling the size criteria

---
+++ src/main/java/com/sallefy/service/dto/criteria/BaseCriteria.java
@@ -0,0 +1,17 @@
+package com.sallefy.service.dto.criteria;
+
+public abstract class BaseCriteria {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer size;
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+}
