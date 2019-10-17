0b828a2cef7bb41c3cdf0602fb31727be981461d
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-10-17 09:57:07 +0200

    Imported player to application

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -19,6 +19,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { PlayerComponent } from 'app/layouts/player/player.component';
 
 @NgModule({
   imports: [
@@ -30,7 +31,15 @@ import { ErrorComponent } from './layouts/error/error.component';
     SallefyEntityModule,
     SallefyAppRoutingModule
   ],
-  declarations: [SfMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+  declarations: [
+    SfMainComponent,
+    NavbarComponent,
+    ErrorComponent,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    PlayerComponent,
+    FooterComponent
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
