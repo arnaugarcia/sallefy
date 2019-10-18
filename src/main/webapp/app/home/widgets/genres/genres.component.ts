76bcd2906f9d831287256f39169913f6332a103c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-10-18 21:00:43 +0200

    Added logic for querying genres

--- src/main/webapp/app/home/widgets/genres/genres.component.ts
+++ src/main/webapp/app/home/widgets/genres/genres.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GenreService } from 'app/shared/services/genre.service';
+import { IGenre } from 'app/shared/model/genre.model';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'sf-genres',
@@ -6,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./genres.component.scss']
 })
 export class GenresComponent implements OnInit {
-  constructor() {}
+  genres: IGenre[] = [];
 
-  ngOnInit() {}
+  constructor(private genreService: GenreService) {}
+
+  ngOnInit() {
+    this.genreService.query().subscribe((response: HttpResponse<IGenre[]>) => {
+      this.genres = response.body;
+    });
+  }
 }
---
+++ src/main/webapp/app/shared/services/genre.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { SERVER_API_URL } from 'app/app.constants';
+import { createRequestOption } from 'app/shared/util/request-util';
+import { IGenre } from 'app/shared/model/genre.model';
+
+type EntityResponseType = HttpResponse<IGenre>;
+type EntityArrayResponseType = HttpResponse<IGenre[]>;
+
+@Injectable({ providedIn: 'root' })
+export class GenreService {
+  public resourceUrl = SERVER_API_URL + 'api/genres';
+
+  constructor(protected http: HttpClient) {}
+
+  create(genre: IGenre): Observable<EntityResponseType> {
+    return this.http.post<IGenre>(this.resourceUrl, genre, { observe: 'response' });
+  }
+
+  update(genre: IGenre): Observable<EntityResponseType> {
+    return this.http.put<IGenre>(this.resourceUrl, genre, { observe: 'response' });
+  }
+
+  find(id: number): Observable<EntityResponseType> {
+    return this.http.get<IGenre>(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  }
+
+  query(req?: any): Observable<EntityArrayResponseType> {
+    const options = createRequestOption(req);
+    return this.http.get<IGenre[]>(this.resourceUrl, { params: options, observe: 'response' });
+  }
+
+  delete(id: number): Observable<HttpResponse<any>> {
+    return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  }
+}
