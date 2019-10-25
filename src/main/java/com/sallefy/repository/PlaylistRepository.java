c495249130b4c49533e17dfb9237213c412953f0
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-10-25 14:43:08 +0200

    Removed entity graph for playlists

--- src/main/java/com/sallefy/repository/PlaylistRepository.java
+++ src/main/java/com/sallefy/repository/PlaylistRepository.java
@@ -1,9 +1,10 @@
 package com.sallefy.repository;
+
 import com.sallefy.domain.Playlist;
-import com.sallefy.service.dto.PlaylistDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -16,7 +17,6 @@ import java.util.Optional;
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 
-    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist where playlist.user.login = ?#{principal.username}")
     List<Playlist> findByUserIsCurrentUser();
 
@@ -24,27 +24,21 @@ public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
         countQuery = "select count(distinct playlist) from Playlist playlist")
     Page<Playlist> findAllWithEagerRelationships(Pageable pageable);
 
-    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select distinct playlist from Playlist playlist left join fetch playlist.tracks")
     List<Playlist> findAllWithEagerRelationships();
 
-    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select distinct playlist from Playlist playlist left join fetch playlist.tracks where playlist.publicAccessible = true")
     List<Playlist> findAllWithEagerRelationshipsAndPublicAccessibleTrue();
 
-    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist left join fetch playlist.tracks where playlist.id =:id")
     Optional<Playlist> findOneWithEagerRelationships(@Param("id") Long id);
 
-    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist where playlist.user.login = ?#{principal.username} and playlist.id = :id")
     Optional<Playlist> findByUserIsCurrentUserAndId(@Param("id") Long id);
 
-    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist where playlist.user.login = :login")
     List<Playlist> findAllByUserLogin(@Param("login") String login);
 
-    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist where playlist.user.login = :login and playlist.publicAccessible = true")
     List<Playlist> findAllByUserLoginAndPublicAccessibleTrue(@Param("login") String login);
 }
