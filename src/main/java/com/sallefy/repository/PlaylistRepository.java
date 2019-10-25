f075a81afa2ab293b14e7feea497f464801be0ff
Arnau Garcia <agarciag@opentrends.net>
2019-10-25 08:40:14 +0200

    Added entity graph for all methods in repository

--- src/main/java/com/sallefy/repository/PlaylistRepository.java
+++ src/main/java/com/sallefy/repository/PlaylistRepository.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 
+    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist where playlist.user.login = ?#{principal.username}")
     List<Playlist> findByUserIsCurrentUser();
 
@@ -31,15 +32,19 @@ public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
     @Query("select distinct playlist from Playlist playlist left join fetch playlist.tracks where playlist.publicAccessible = true")
     List<Playlist> findAllWithEagerRelationshipsAndPublicAccessibleTrue();
 
+    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist left join fetch playlist.tracks where playlist.id =:id")
     Optional<Playlist> findOneWithEagerRelationships(@Param("id") Long id);
 
+    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist where playlist.user.login = ?#{principal.username} and playlist.id = :id")
     Optional<Playlist> findByUserIsCurrentUserAndId(@Param("id") Long id);
 
+    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist where playlist.user.login = :login")
     List<Playlist> findAllByUserLogin(@Param("login") String login);
 
+    @EntityGraph("playlist-entity-graph-with-user-tracks")
     @Query("select playlist from Playlist playlist where playlist.user.login = :login and playlist.publicAccessible = true")
     List<Playlist> findAllByUserLoginAndPublicAccessibleTrue(@Param("login") String login);
 }
