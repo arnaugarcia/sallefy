03043f0b899c79332134afc62e7600e40d6c5f69
Arnau Garcia <agarciag@opentrends.net>
2019-10-25 09:39:55 +0200

    Removed unused imports

--- src/main/java/com/sallefy/service/impl/PlaylistServiceImpl.java
+++ src/main/java/com/sallefy/service/impl/PlaylistServiceImpl.java
@@ -17,8 +17,6 @@ import com.sallefy.service.mapper.PlaylistMapper;
 import com.sallefy.service.mapper.TrackMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
