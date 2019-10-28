6c4437baa604998e354b356ac3efa85d9df07fa4
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-10-28 12:58:45 +0100

    Added JpaSpecification to playlist repository

--- src/main/java/com/sallefy/repository/PlaylistRepository.java
+++ src/main/java/com/sallefy/repository/PlaylistRepository.java
@@ -4,6 +4,7 @@ import com.sallefy.domain.Playlist;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
