9a71eb08f686274ef10d39ab5c5964ce23e4ace2
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-10-31 11:09:12 +0100

    Added elastic search database configuration

--- src/main/java/com/sallefy/config/DatabaseConfiguration.java
+++ src/main/java/com/sallefy/config/DatabaseConfiguration.java
@@ -1,12 +1,9 @@
 package com.sallefy.config;
 
-import io.github.jhipster.config.JHipsterConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories("com.sallefy.repository")
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 @EnableTransactionManagement
+@EnableElasticsearchRepositories("com.sallefy.repository.search")
 public class DatabaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
