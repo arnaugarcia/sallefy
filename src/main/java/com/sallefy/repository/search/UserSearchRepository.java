19b006c740f5b769b09950a0388d621fe7a7dade
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-10-31 11:24:56 +0100

    Created elastic search repository for User

---
+++ src/main/java/com/sallefy/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.sallefy.repository.search;
+
+import com.sallefy.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
---
+++ src/main/java/com/sallefy/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package com.sallefy.repository.search;
