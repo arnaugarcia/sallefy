fd48da41c5cfc8e48f3a0ef6c942634eae2b18dd
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-11-04 12:46:22 +0100

    Simplified transformation of search objects

--- src/main/java/com/sallefy/service/impl/SearchServiceImpl.java
+++ src/main/java/com/sallefy/service/impl/SearchServiceImpl.java
@@ -22,6 +22,7 @@ public class SearchServiceImpl implements SearchService {
     private final UserSearchRepository userSearchRepository;
 
     private final PlaylistSearchRepository playlistSearchRepository;
+
     private final PlaylistMapper playlistMapper;
 
     public SearchServiceImpl(UserSearchRepository userSearchRepository,
@@ -34,18 +35,21 @@ public class SearchServiceImpl implements SearchService {
 
     @Override
     public SearchDTO search(QueryStringQueryBuilder query) {
-        final Iterable<User> usersIterator = userSearchRepository.search(query);
-        final Iterable<Playlist> playlists = playlistSearchRepository.search(query);
-
-        List<Playlist> playlistList = new ArrayList<>();
-        playlists.forEach(playlistList::add);
-
-        List<User> users = new ArrayList<>();
-        usersIterator.forEach(users::add);
-
         SearchDTO searchDTO = new SearchDTO();
 
-        searchDTO.setPlaylists(playlistMapper.toDto(playlistList));
+        List<User> users = new ArrayList<>();
+        List<Playlist> playlists = new ArrayList<>();
+
+        userSearchRepository.search(query)
+            .spliterator()
+            .forEachRemaining(users::add);
+
+        playlistSearchRepository.search(query)
+            .spliterator()
+            .forEachRemaining(playlists::add);
+
+
+        searchDTO.setPlaylists(playlistMapper.toDto(playlists));
         searchDTO.setUsers(users.stream()
             .map(UserSimplifyedDTO::new)
             .collect(toList()));
