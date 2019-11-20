1edb9e5551a2f6f0a761880f44f727bbc77838f6
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-11-20 11:00:28 +0100

    Added documentation for search service

--- src/main/java/com/sallefy/service/SearchService.java
+++ src/main/java/com/sallefy/service/SearchService.java
@@ -5,6 +5,12 @@ import org.elasticsearch.index.query.QueryStringQueryBuilder;
 
 public interface SearchService {
 
+    /**
+     * Search in the datastore of Elasticsearch by QueryString
+     *
+     * @param query the string to find
+     * @return a searchDTO object containing all the data
+     */
     SearchDTO search(QueryStringQueryBuilder query);
 
 }
