9164c380576e770b3f02efd3888d3f552f4f69dd
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-12-18 14:45:07 +0100

    Added validator if the value is null

--- src/main/java/com/sallefy/service/dto/constraints/LongitudeValidator.java
+++ src/main/java/com/sallefy/service/dto/constraints/LongitudeValidator.java
@@ -8,7 +8,11 @@ public class LongitudeValidator implements ConstraintValidator<Longitude, Double
     public void initialize(Longitude constraint) {
     }
 
-    public boolean isValid(Double latitude, ConstraintValidatorContext context) {
-        return (latitude > 180 || latitude < - 180);
+    public boolean isValid(Double longitude, ConstraintValidatorContext context) {
+        return longitude != null && isInRange(longitude);
+    }
+
+    private boolean isInRange(Double longitude) {
+        return longitude > 180 || longitude < - 180;
     }
 }
