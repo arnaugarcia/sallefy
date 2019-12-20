5b433ec5c74c8a24797db1e23081af518feec04c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-12-20 13:36:50 +0100

    Added markers service to markers resource

--- src/main/java/com/sallefy/web/rest/MarkersResource.java
+++ src/main/java/com/sallefy/web/rest/MarkersResource.java
@@ -1,21 +1,15 @@
 package com.sallefy.web.rest;
 
-import com.sallefy.service.FollowService;
-import com.sallefy.service.PlaylistService;
-import com.sallefy.service.TrackService;
+import com.sallefy.service.MarkerService;
 import com.sallefy.service.dto.MarkerDTO;
-import com.sallefy.service.dto.PlaylistDTO;
-import com.sallefy.service.dto.TrackDTO;
-import com.sallefy.service.dto.UserSimplifiedDTO;
+import com.sallefy.service.dto.criteria.MarkerCriteriaDTO;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +29,11 @@ public class MarkersResource {
 
     private final Logger log = LoggerFactory.getLogger(MarkersResource.class);
 
+    private final MarkerService markerService;
+
+    public MarkersResource(MarkerService markerService) {
+        this.markerService = markerService;
+    }
 
     /**
      * {@code GET  /me/markers} : get all the markers
@@ -48,10 +47,10 @@ public class MarkersResource {
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "Successful operation")
     })
-    @GetMapping("/me/markers")
-    public ResponseEntity<List<MarkerDTO>> getOwnTracks() {
+    @GetMapping("/markers")
+    public ResponseEntity<List<MarkerDTO>> findMarkers(MarkerCriteriaDTO criteria) {
         log.debug("REST request to get all Tracks");
-        return ok(trackService.findAllByCurrentUser());
+        return ok(markerService.findByCriteria(criteria));
     }
 
 }
