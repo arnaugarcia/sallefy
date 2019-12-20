b6007c0b084efe7c674179f7c4edfcfd4bff697d
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-12-20 13:38:56 +0100

    Create MarkerCriteriaDTO for getting user criteria

---
+++ src/main/java/com/sallefy/service/dto/criteria/MarkerCriteriaDTO.java
@@ -0,0 +1,98 @@
+package com.sallefy.service.dto.criteria;
+
+import com.sallefy.service.dto.constraints.AllOrNone;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@AllOrNone({"latitude", "longitude", "radius"})
+public class MarkerCriteriaDTO extends BaseCriteria implements Serializable {
+
+    private Double latitude;
+    private Double longitude;
+    private Integer radius;
+    private Boolean me;
+    private Long username;
+    private Long trackId;
+    private String genre;
+
+    public MarkerCriteriaDTO() {
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Integer radius) {
+        this.radius = radius;
+    }
+
+    public Boolean getMe() {
+        return me;
+    }
+
+    public void setMe(Boolean me) {
+        this.me = me;
+    }
+
+    public Long getUsername() {
+        return username;
+    }
+
+    public void setUsername(Long username) {
+        this.username = username;
+    }
+
+    public Long getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(Long trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MarkerCriteriaDTO)) return false;
+        MarkerCriteriaDTO that = (MarkerCriteriaDTO) o;
+        return Objects.equals(latitude, that.latitude) &&
+            Objects.equals(longitude, that.longitude) &&
+            Objects.equals(radius, that.radius) &&
+            Objects.equals(me, that.me) &&
+            Objects.equals(username, that.username) &&
+            Objects.equals(trackId, that.trackId) &&
+            Objects.equals(genre, that.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitude, longitude, radius, me, username, trackId, genre);
+    }
+
+
+}
