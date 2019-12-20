75e5e4124b05ad29c0fde57068ac24c79e9c644e
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-12-20 13:39:19 +0100

    Created MarkerService and implemented base logic

---
+++ src/main/java/com/sallefy/service/MarkerService.java
@@ -0,0 +1,10 @@
+package com.sallefy.service;
+
+import com.sallefy.service.dto.MarkerDTO;
+import com.sallefy.service.dto.criteria.MarkerCriteriaDTO;
+
+public interface MarkerService extends QueryService<MarkerDTO, MarkerCriteriaDTO>{
+
+
+
+}
