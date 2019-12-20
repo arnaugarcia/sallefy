5830fd211095872f92d97f8bdc1d61695fe3506e
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-12-20 14:08:43 +0100

    Extended JPA Specification from playback repository

--- src/main/java/com/sallefy/repository/PlaybackRepository.java
+++ src/main/java/com/sallefy/repository/PlaybackRepository.java
@@ -1,6 +1,9 @@
 package com.sallefy.repository;
+
 import com.sallefy.domain.Playback;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +13,7 @@ import java.util.List;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface PlaybackRepository extends JpaRepository<Playback, Long> {
+public interface PlaybackRepository extends JpaRepository<Playback, Long>, JpaSpecificationExecutor<Playback> {
 
     @Query("select playback from Playback playback where playback.user.login = ?#{principal.username}")
     List<Playback> findByUserIsCurrentUser();
