c58b6580f5d1a5a6307a7fd6595cd973d204afce
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-01-02 11:36:21 +0100

    Added latitude and longitude null validators

--- src/main/java/com/sallefy/service/dto/constraints/LatitudeValidator.java
+++ src/main/java/com/sallefy/service/dto/constraints/LatitudeValidator.java
@@ -3,11 +3,13 @@ package com.sallefy.service.dto.constraints;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import static org.springframework.util.StringUtils.isEmpty;
+
 public class LatitudeValidator implements ConstraintValidator<Latitude, Double> {
     public void initialize(Latitude constraint) {
     }
 
     public boolean isValid(Double latitude, ConstraintValidatorContext context) {
-        return latitude > -90 && latitude < 90;
+        return !isEmpty(latitude) && latitude > -90 && latitude < 90;
     }
 }
--- src/main/java/com/sallefy/service/dto/constraints/LongitudeValidator.java
+++ src/main/java/com/sallefy/service/dto/constraints/LongitudeValidator.java
@@ -3,11 +3,13 @@ package com.sallefy.service.dto.constraints;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import static org.springframework.util.StringUtils.isEmpty;
+
 public class LongitudeValidator implements ConstraintValidator<Longitude, Double> {
     public void initialize(Longitude constraint) {
     }
 
     public boolean isValid(Double longitude, ConstraintValidatorContext context) {
-        return longitude < 180 && longitude > -180;
+        return !isEmpty(longitude) && longitude < 180 && longitude > -180;
     }
 }
