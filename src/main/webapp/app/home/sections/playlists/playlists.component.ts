e43faab86c12cc72e0407ee8f2d2765be27646ab
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-01-07 09:44:31 +0100

    Added owl carrousel for playlists

--- src/main/webapp/app/home/sections/playlists/playlists.component.ts
+++ src/main/webapp/app/home/sections/playlists/playlists.component.ts
@@ -6,6 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playlists.component.scss']
 })
 export class PlaylistsComponent implements OnInit {
+  customOptions: any = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
