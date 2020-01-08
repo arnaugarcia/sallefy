637831315cf075695f8c46f008a784fa697ad458
Arnau Garcia <agarciag@opentrends.net>
2020-01-08 14:11:45 +0100

    Deleted image check directive

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -5,18 +5,10 @@ import { SfAlertComponent } from './alert/alert.component';
 import { SfAlertErrorComponent } from './alert/alert-error.component';
 import { SfLoginModalComponent } from './login/login.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
-import { ImageCheckDirective } from 'app/shared/directives/image-check.directive';
 
 @NgModule({
   imports: [SallefySharedLibsModule],
-  declarations: [
-    FindLanguageFromKeyPipe,
-    SfAlertComponent,
-    SfAlertErrorComponent,
-    SfLoginModalComponent,
-    HasAnyAuthorityDirective,
-    ImageCheckDirective
-  ],
+  declarations: [FindLanguageFromKeyPipe, SfAlertComponent, SfAlertErrorComponent, SfLoginModalComponent, HasAnyAuthorityDirective],
   entryComponents: [SfLoginModalComponent],
   exports: [
     SallefySharedLibsModule,
@@ -24,8 +16,7 @@ import { ImageCheckDirective } from 'app/shared/directives/image-check.directive
     SfAlertComponent,
     SfAlertErrorComponent,
     SfLoginModalComponent,
-    HasAnyAuthorityDirective,
-    ImageCheckDirective
+    HasAnyAuthorityDirective
   ]
 })
 export class SallefySharedModule {}
