f875a2bfa4c50ece420bc4a9b88b11d968149b31
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-01-10 14:54:35 +0100

    Set DEBUG_INFO_ENABLED to false

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -56,7 +56,7 @@ module.exports = (options) => ({
                 BUILD_TIMESTAMP: `'${new Date().getTime()}'`,
                 // APP_VERSION is passed as an environment variable from the Gradle / Maven build tasks.
                 VERSION: `'${process.env.hasOwnProperty('APP_VERSION') ? process.env.APP_VERSION : 'DEV'}'`,
-                DEBUG_INFO_ENABLED: options.env === 'development',
+                DEBUG_INFO_ENABLED: false,
                 // The root URL for API calls, ending with a '/' - for example: `"https://www.jhipster.tech:8081/myservice/"`.
                 // If this URL is left empty (""), then it will be relative to the current context.
                 // If you use an API server, in `prod` mode, you will need to enable CORS
