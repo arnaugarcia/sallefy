c28bbe80f8d9eb9991919d0f43c4bfd74a913c57
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-01-16 14:43:07 +0100

    Created and extracted widgets to a module

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -5,13 +5,10 @@ import { SallefySharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 import { TracksComponent } from './sections/tracks/tracks.component';
-import { GenresComponent } from './widgets/genres/genres.component';
-import { ArtistsComponent } from './widgets/artists/artists.component';
-import { TopTracksComponent } from './widgets/top-tracks/top-tracks.component';
 import { PlaylistsComponent } from './sections/playlists/playlists.component';
 
 @NgModule({
   imports: [SallefySharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [TracksComponent, HomeComponent, GenresComponent, ArtistsComponent, TopTracksComponent, PlaylistsComponent]
+  declarations: [TracksComponent, HomeComponent, PlaylistsComponent]
 })
 export class SallefyHomeModule {}
---
+++ src/main/webapp/app/layouts/widgets/widgets.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sf-widgets',
+  templateUrl: './widgets.component.html',
+  styleUrls: ['./widgets.component.scss']
+})
+export class WidgetsComponent {}
