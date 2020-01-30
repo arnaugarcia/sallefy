e33dbdef52362b765928b1a3d8e6ad7a3eb25cec
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-01-30 16:53:36 +0100

    Removed old logic and started with new one

--- src/main/webapp/app/layouts/widgets/widgets.component.ts
+++ src/main/webapp/app/layouts/widgets/widgets.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, OnDestroy, OnInit, Type, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ComponentFactoryResolver, OnDestroy, OnInit, Type, ViewChild, ViewContainerRef } from '@angular/core';
 import { WidgetsDirective } from 'app/layouts/widgets/widgets.directive';
 import { GenresComponent } from 'app/layouts/widgets/genres/genres.component';
 import { WidgetsService } from 'app/layouts/widgets/widgets.service';
@@ -12,31 +12,26 @@ import { TopTracksComponent } from 'app/layouts/widgets/top-tracks/top-tracks.co
 })
 // TODO: Create a WidgetBase in order to inherit reload() method to all widgets
 // TODO: Make sure that the widgets doesn't create a memory leak
-export class WidgetsComponent implements OnInit, OnDestroy {
+export class WidgetsComponent implements OnInit, OnDestroy, AfterViewInit {
   public widgets: Type<WidgetBase>[] = [];
+
   @ViewChild(WidgetsDirective, { static: true })
-  private widgetsAnchor?: WidgetsDirective;
+  private anchorPoint: WidgetsDirective;
+
+  private widgetContainerRef: ViewContainerRef;
 
   constructor(private componentFactoryResolver: ComponentFactoryResolver, private widgetsService: WidgetsService) {}
 
   ngOnDestroy(): void {}
 
   ngOnInit(): void {
-    console.warn('Widgets init');
     this.widgetsService.widgets$.subscribe((widgets: Type<WidgetBase>[]) => {
-      console.warn('New widgets loaded!', widgets);
-      widgets.forEach((widget: Type<WidgetBase>) => {
-        if (!this.widgets.includes(widget)) {
-          this.loadComponent(widget);
-        } else {
-          const difference = this.widgets.filter(x => !widgets.includes(x));
-          if (difference.length) {
-            this.removeComponent(difference[0]);
-            // difference.forEach((deleteWidget: Type<WidgetBase>) => this.widgets.slice(this.widgets.indexOf(deleteWidget), 1));
-          }
-        }
-      });
+      this.widgets = widgets;
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.widgetContainerRef = this.anchorPoint.viewContainerRef;
   }
 
   removeTracks(): void {
@@ -57,14 +52,10 @@ export class WidgetsComponent implements OnInit, OnDestroy {
 
   private loadComponent(widget: Type<WidgetBase>): void {
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(widget);
-    const viewContainerRef = this.widgetsAnchor.viewContainerRef;
-    viewContainerRef.createComponent(componentFactory);
-    this.widgets.push(widget);
+    this.widgetContainerRef.createComponent(componentFactory);
   }
 
   private removeComponent(widget: Type<WidgetBase>): void {
-    const viewContainerRef = this.widgetsAnchor.viewContainerRef;
-    viewContainerRef.remove(this.widgets.indexOf(widget));
-    this.widgets.splice(this.widgets.indexOf(widget), 1);
+    this.widgetContainerRef.remove(this.widgets.indexOf(widget));
   }
 }
