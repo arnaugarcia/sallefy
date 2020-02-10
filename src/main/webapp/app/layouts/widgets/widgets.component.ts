f95eed0975200c48390f0c0ac57e9ccf202ba44f
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-02-10 17:14:19 +0100

    Removed testing code

--- src/main/webapp/app/layouts/widgets/widgets.component.ts
+++ src/main/webapp/app/layouts/widgets/widgets.component.ts
@@ -1,11 +1,8 @@
-import { AfterViewInit, Component, ComponentFactoryResolver, OnDestroy } from '@angular/core';
-import { WidgetsService } from 'app/layouts/widgets/widgets.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'sf-widgets',
   templateUrl: './widgets.component.html',
   styleUrls: ['./widgets.component.scss']
 })
-export class WidgetsComponent implements AfterViewInit, OnDestroy {
-  constructor(private componentFactoryResolver: ComponentFactoryResolver, private widgetsService: WidgetsService) {}
-}
+export class WidgetsComponent {}
