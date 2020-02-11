694973fa313a8cc3a593897ae859bf6f9665064b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-02-11 16:33:13 +0100

    Lited track model

--- src/main/webapp/app/shared/model/track.model.ts
+++ src/main/webapp/app/shared/model/track.model.ts
@@ -1,42 +1,26 @@
-import { Moment } from 'moment';
 import { IGenre } from 'app/shared/model/genre.model';
-import { IPlaylist } from 'app/shared/model/playlist.model';
-import { IAlbum } from 'app/shared/model/album.model';
+import { IUser, IUserSimplified } from 'app/core/user/user.model';
 
 export interface ITrack {
   id?: number;
   name?: string;
-  rating?: string;
   url?: string;
-  popularity?: string;
   thumbnail?: string;
-  createdAt?: Moment;
-  released?: Moment;
   duration?: number;
   color?: string;
-  userLogin?: string;
-  userId?: number;
+  owner?: IUserSimplified;
   genres?: IGenre[];
-  playlists?: IPlaylist[];
-  albums?: IAlbum[];
 }
 
 export class Track implements ITrack {
   constructor(
     public id?: number,
     public name?: string,
-    public rating?: string,
     public url?: string,
-    public popularity?: string,
     public thumbnail?: string,
-    public createdAt?: Moment,
-    public released?: Moment,
     public duration?: number,
     public color?: string,
-    public userLogin?: string,
-    public userId?: number,
-    public genres?: IGenre[],
-    public playlists?: IPlaylist[],
-    public albums?: IAlbum[]
+    public owner?: IUser,
+    public genres?: IGenre[]
   ) {}
 }
