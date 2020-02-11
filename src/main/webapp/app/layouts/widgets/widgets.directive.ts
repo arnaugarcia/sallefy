a88c6d3e9f520b1a51ef1c7457736b7d3251c675
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-02-11 16:33:55 +0100

    Removed unused directive

--- src/main/webapp/app/layouts/widgets/widgets.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-  selector: '[sfWidgets]'
-})
-export class WidgetsDirective {
-  constructor(public viewContainerRef: ViewContainerRef) {}
-}
