8ad17e1590cac713abac3afc3942024037810cf7
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-02-18 19:22:35 +0100

    Getting info from current track

--- src/main/webapp/app/layouts/player/player.component.ts
+++ src/main/webapp/app/layouts/player/player.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { PlayerService } from 'app/layouts/player/player.service';
 import { VgAPI } from 'videogular2/compiled/src/core/services/vg-api';
-import { ITrack } from 'app/shared/model/track.model';
+import { ITrack, Track } from 'app/shared/model/track.model';
 
 @Component({
   selector: 'sf-player',
@@ -11,12 +11,17 @@ import { ITrack } from 'app/shared/model/track.model';
 export class PlayerComponent implements OnInit, AfterViewInit {
   public api: VgAPI | undefined;
   public tracks: ITrack[] = [];
+  public currentTrack: ITrack = new Track();
 
   constructor(private playerService: PlayerService) {}
 
   ngOnInit(): void {
     this.playerService.queue$.subscribe((tracks: ITrack[]) => {
       this.tracks = tracks;
+      if (this.tracks.length !== 0) {
+        this.currentTrack = this.tracks[0];
+        this.playCurrentLoadedTrack();
+      }
     });
   }
 
@@ -26,6 +31,10 @@ export class PlayerComponent implements OnInit, AfterViewInit {
 
   onPlayerReady(api: VgAPI): void {
     this.api = api;
+  }
+
+  playCurrentLoadedTrack(): void {
+    if (this.api) this.api.play();
   }
 
   ngAfterViewInit(): void {}
