1ba4244eddbbec91f8b365de8dc1163b15c256c1
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-04-10 13:22:40 +0200

    Created adn moved logic to track component

---
+++ src/main/webapp/app/home/sections/tracks/track/track.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ITrack, Track } from 'app/shared/model/track.model';
+
+@Component({
+  selector: 'sf-track',
+  templateUrl: './track.component.html',
+  styleUrls: ['./track.component.scss']
+})
+export class TrackComponent implements OnInit {
+  @Input()
+  public track: ITrack = new Track();
+
+  public hover = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  play(track: ITrack): void {}
+}
