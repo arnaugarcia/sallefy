f9ab34e8e33730703c8bb926ec64ecb29d956360
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-04-10 21:35:59 +0200

    Adding more template logic for playlist detail component

--- src/main/webapp/app/playlist/playlist-detail/playlist-detail.component.ts
+++ src/main/webapp/app/playlist/playlist-detail/playlist-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IPlaylist, Playlist } from 'app/shared/model/playlist.model';
 import { ActivatedRoute } from '@angular/router';
+import { PlaylistService } from 'app/shared/services/playlist.service';
 
 @Component({
   selector: 'sf-playlist-detail',
@@ -10,11 +11,15 @@ import { ActivatedRoute } from '@angular/router';
 export class PlaylistDetailComponent implements OnInit {
   public playlist: IPlaylist = new Playlist();
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, private playlistService: PlaylistService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ playlist }) => {
       this.playlist = playlist;
     });
+  }
+
+  toggleFollowPlaylist(): void {
+    this.playlistService;
   }
 }
