2633460b24dce16a45e9fa910d03e5de82629588
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-04-11 20:49:54 +0200

    Added logic for checking if current user is the owner

--- src/main/webapp/app/playlist/playlist-detail/playlist-detail.component.ts
+++ src/main/webapp/app/playlist/playlist-detail/playlist-detail.component.ts
@@ -1,25 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IPlaylist, Playlist } from 'app/shared/model/playlist.model';
 import { ActivatedRoute } from '@angular/router';
 import { PlaylistService } from 'app/shared/services/playlist.service';
 import { HttpResponse } from '@angular/common/http';
 import { IFollow } from 'app/shared/model/follow.model';
 import { PlayerService } from 'app/layouts/player/player.service';
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/user/account.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sf-playlist-detail',
   templateUrl: './playlist-detail.component.html',
   styleUrls: ['./playlist-detail.component.scss']
 })
-export class PlaylistDetailComponent implements OnInit {
+export class PlaylistDetailComponent implements OnInit, OnDestroy {
   public playlist: IPlaylist = new Playlist();
+  public currentUser: Account | undefined;
+  private accountSubscription: Subscription = new Subscription();
 
-  constructor(protected activatedRoute: ActivatedRoute, private playlistService: PlaylistService, private playerService: PlayerService) {}
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    private playlistService: PlaylistService,
+    private playerService: PlayerService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ playlist }) => {
       this.playlist = playlist;
     });
+    this.accountSubscription = this.accountService.identity().subscribe((account: Account | null) => {
+      if (account) {
+        this.currentUser = account;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.accountSubscription.unsubscribe();
   }
 
   toggleFollowPlaylist(): void {
@@ -39,5 +58,16 @@ export class PlaylistDetailComponent implements OnInit {
       return 0;
     }
     return Math.floor(this.playlist.tracks.reduce((total, track) => (total += track.duration ? track.duration : 0), 0) / 60);
+  }
+
+  hasTracks(): boolean | undefined {
+    return this.playlist.tracks && this.playlist.tracks.length !== 0;
+  }
+
+  currentUserIsTheOwner(): boolean {
+    if (!this.playlist.owner || !this.currentUser) {
+      return false;
+    }
+    return this.playlist.owner.login === this.currentUser.login;
   }
 }
