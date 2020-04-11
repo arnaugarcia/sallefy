d4e7a63d4bf91d2120a09202db8173a0cb67598d
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-04-11 20:12:51 +0200

    Created function for getting all the songs duration

--- src/main/webapp/app/playlist/playlist-detail/playlist-detail.component.ts
+++ src/main/webapp/app/playlist/playlist-detail/playlist-detail.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { PlaylistService } from 'app/shared/services/playlist.service';
 import { HttpResponse } from '@angular/common/http';
 import { IFollow } from 'app/shared/model/follow.model';
+import { PlayerService } from 'app/layouts/player/player.service';
 
 @Component({
   selector: 'sf-playlist-detail',
@@ -13,7 +14,7 @@ import { IFollow } from 'app/shared/model/follow.model';
 export class PlaylistDetailComponent implements OnInit {
   public playlist: IPlaylist = new Playlist();
 
-  constructor(protected activatedRoute: ActivatedRoute, private playlistService: PlaylistService) {}
+  constructor(protected activatedRoute: ActivatedRoute, private playlistService: PlaylistService, private playerService: PlayerService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ playlist }) => {
@@ -27,5 +28,16 @@ export class PlaylistDetailComponent implements OnInit {
         this.playlist.followed = response.body.followed;
       }
     });
+  }
+
+  play(): void {
+    this.playerService.playPlaylist(this.playlist);
+  }
+
+  getTotalSongsDuration(): number {
+    if (!this.playlist.tracks) {
+      return 0;
+    }
+    return Math.floor(this.playlist.tracks.reduce((total, track) => (total += track.duration ? track.duration : 0), 0) / 60);
   }
 }
