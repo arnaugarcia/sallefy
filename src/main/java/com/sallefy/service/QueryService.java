642aa01898c6451a71e25b6b17cc28b554cdd040
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2020-04-17 18:51:42 +0200

    Changed findByCriteria to return a Page instead of a List

--- src/main/java/com/sallefy/service/QueryService.java
+++ src/main/java/com/sallefy/service/QueryService.java
@@ -1,12 +1,12 @@
 package com.sallefy.service;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.io.Serializable;
-import java.util.List;
 
 public interface QueryService<D, C extends Serializable> {
 
-    List<D> findByCriteria(C criteria, Pageable pageable);
+    Page<D> findByCriteria(C criteria, Pageable pageable);
 
 }
